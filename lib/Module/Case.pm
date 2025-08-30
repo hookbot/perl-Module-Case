f5646188d91524f6bb26a0fef1648f4b68788d7e
Rob Brown <bbb@cpan.org>
2025-08-30 01:50:28 -0600

    Implement forced case-sensitive module loading functionality.

--- lib/Module/Case.pm
+++ lib/Module/Case.pm
@@ -4,83 +4,150 @@ use 5.008000;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Module::Case ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.03';
 
-require XSLoader;
-XSLoader::load('Module::Case', $VERSION);
+our $sensitive_modules;
 
-# Preloaded methods go here.
+sub import {
+    my $class = shift;
+    foreach (@_) {
+        $sensitive_modules->{$_} = $_;
+    }
+    return;
+}
+
+our $inc_sniffer = sub {
+    # Special @INC hook to only load modules with exact case match.
+    # This is particularly useful on case insensitive file systems.
+    my ($self, $filename) = @_;
+    # Ignore full path filenames
+    return undef if $filename =~ m{^/};
+    # Calculate package name
+    my $pkg = $filename;
+    $pkg =~ s/\.pm$//;
+    $pkg =~ s{/+}{::}g;
+    # For efficiency purposes, skip module unless it's one of the special case sensitive packages flagged to load case-sensitively.
+    delete $sensitive_modules->{$pkg} or return undef;
+
+    # Skip the directories before me since they've already been tried and obviously didn't find the file
+    my $keep = 0;
+    # Only look through regular directories after myself but ignore CODEREFs (such as myself) in @INC
+    my @scan = grep { $keep = 1 if $_ eq $self; !ref $_ and $keep; } @INC;
+    if (!keys %$sensitive_modules) {
+        # If this was the last sensitive module, restore @INC without me
+        $sensitive_modules = undef;
+        @INC = grep { $_ ne $self } @INC;
+    }
+    my $found_wrong_case = 0;
+    foreach my $dir (grep { !ref $_ } @scan) {
+        if (open my $fh, "<", "$dir/$filename") {
+            # Found a matching file but might not have same case.
+            # Take a quick peek to packe sure the case matches too.
+            my $contents = join "", <$fh>;
+            if ($contents =~ /^\s*package\s+\Q$pkg\E\s*;/m) {
+                 # Smells like a pretty good package.
+                 # Case matches case exactly.
+                 # So rewind and return this handle.
+                 seek($fh, 0, 0);
+                 $INC{$filename} = "$dir/$filename";
+                 return $fh;
+            }
+            else {
+                # Looks like we found a file with the wrong case, so ignore it.
+                $found_wrong_case ||= "$dir/$filename";
+            }
+            close $fh;
+        }
+    }
+    # Couldn't find the real module
+    if ($found_wrong_case) {
+        # Found a case insensitive match but did NOT find an exact match.
+        # We need to block Perl from continuing along @INC or else it will find the bad guy too.
+        die "Can't locate $filename in @INC except for decoy with wrong case: $found_wrong_case\n";
+    }
+    # Can't even find a case-insensitive match, so just continue and let Perl try
+    return undef;
+};
+
+our $already_injected;
+if (!$already_injected++) {
+    # Never injected into @INC yet
+    unshift @INC, $inc_sniffer;
+    $sensitive_modules ||= {};
+}
 
 1;
+
 __END__
-# Below is stub documentation for your module. You'd better edit it!
+
+=pod
 
 =head1 NAME
 
-Module::Case - Perl extension for blah blah blah
+Module::Case - Case sensitive module loading.
 
 =head1 SYNOPSIS
 
-  use Module::Case;
-  blah blah blah
+    # Specify which modules you wish to force the case
+    # to match exactly if ever loaded in the future
+    use Module::Case qw(Config config);
+
+    # Load only Config.pm starting with CAPITAL C
+    # even on case-insensitive file systems and
+    # even if lowercase config.pm is earlier in the @INC.
+    use Config;
+
+    # Now load only the lowercase file
+    use config;
 
 =head1 DESCRIPTION
 
-Stub documentation for Module::Case, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This is really only useful on Operating Systems that support case-insensitive filesystems.
 
-Blah blah blah.
+This modules injects a special CODEREF at the beginning of @INC that
+performs a pre-check on each matching file within @INC to ensure the
+case is exactly correct instead of allowing Perl to blindly compile
+any file that smells like the module wishing to load.
 
-=head2 EXPORT
+If the specified module doesn't exactly match the case of how it was
+requested, then the "require" or "use" will die with an error
+instead of gleefully loading any module case-insensitively matched.
 
-None by default.
+=head1 CAVEATS
 
+The module being loaded must contain the exact string of the package
+matching what has been flagged for sensitively. For example:
 
+    use Module::Case qw(Exact::Module::Name);
+    use Exact::Module::Name;
+    # -or-
+    require Exact::Module::Name;
 
-=head1 SEE ALSO
+Then the contents of "Exact/Module/Name.pm" must contain
+"package Exact::Module::Name" somewhere in the pm file
+or else it should fail to load.
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+If there are multiple different modules that match case-insensitvely
+and you wish to load more than one of these, then it would be safer
+to specify ALL of these variations in the import line, such as:
 
-If you have a mailing list set up for your module, mention it here.
+    use Module::Case qw(cwd Cwd);
+    use cwd;
+    use Cwd;
 
-If you have a web site set up for your module, mention it here.
+This is because once a case-sensitive module is loaded successfully,
+then it is immediately removed from the import whitelist.
+Technically, you only need to specify the module that is latest in
+the @INC, but if this order ever changes in the future, then it will
+incorrectly load the FIRST case-insensitive match in @INC.
 
 =head1 AUTHOR
 
-Rob Brown, E<lt>hookbot@p1s.plx.sd.apple.comE<gt>
+Rob Brown <bbb@cpan.org>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 SEE ALSO
 
-Copyright (C) 2025 by Rob Brown
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.34.1 or,
-at your option, any later version of Perl 5 you may have available.
-
+Similar to Acme::require::case except this doesn't require any
+dependencies and it's quite fast.
 
 =cut
